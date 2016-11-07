a3b5c466ad0c060177c733c06bc52860a6064500
Sarah Dickerson <email": null>
2016-11-07 17:58:38 -0600

    homework

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,29 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+var up = 38;
+var down = 40;
+var upCount = 0;
+var downCount = 0;
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(document).ready(function() {
+  $(document).on('keydown', function(event) {
+    var boardEnd = $('td').length/2;
+      if (event.which === up) {
+        var $t = $('td.active1')
+        $t.removeClass('active1');
+        $t.next().addClass('active1');
+        upCount += 1;
+      } else if (event.which === down) {
+        var $t = $('td.active2')
+        $t.removeClass('active2');
+        $t.next().addClass('active2');
+        downCount += 1;
+      };
+    if (upCount === boardEnd) {
+      alert("Player 1 wins!!")
+      window.location.reload();
+    } else if (downCount === boardEnd) {
+      alert("Player 2 wins!!")
+      window.location.reload();
+    };
+  });
+
 });
