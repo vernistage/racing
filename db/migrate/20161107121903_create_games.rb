bb9edba4673e209afaa2ee4e51f982901efd31fb
Sarah Dickerson <email": null>
2016-11-08 11:31:31 -0600

    change winner column datatype to string

--- db/migrate/20161107121903_create_games.rb
+++ db/migrate/20161107121903_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :name
-      t.integer :winner
+      t.string :winner
 
       t.timestamps(null: false)
     end
